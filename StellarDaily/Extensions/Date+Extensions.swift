ade93ff78f4d9533c83030f93c688dab372e56c4
Oleksandr Yevdokymov <oleksandr.yevdokymov.93@gmail.com>
2026-02-15 00:30:19 +0100

    TimeZone fix

--- StellarDaily/Extensions/Date+Extensions.swift
+++ StellarDaily/Extensions/Date+Extensions.swift
@@ -12,7 +12,7 @@ extension Date {
         let formatter = DateFormatter()
         formatter.calendar = Calendar(identifier: .gregorian)
         formatter.locale = Locale(identifier: "en_US_POSIX")
-        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.timeZone = .current
         formatter.dateFormat = "yyyy-MM-dd"
         return formatter.string(from: self)
     }
